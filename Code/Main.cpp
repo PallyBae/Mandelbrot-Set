4424137b65ec87c2cdb4be9aebcc7a92f413d33e
admin <admin@BCS-Ubuntu-12.lrccd.ad.losrios.edu>
2022-09-21 02:02:17 -0700

    https://github.com/PallyBae/Mandelbrot-Set

--- Code/Main.cpp
+++ Code/Main.cpp
@@ -24,12 +24,13 @@ int main()
     //Construct your window
     RenderWindow window(desktop, "Mandelbrot Set!!", Style::Default);
     //Construct an object of type ComplexPlane
-    class ComplexPlane
-    {
-        private:
+    
+            //NEEDS WORK!!! (1)
+            //NEEDS WORK!!! (1)
+            //NEEDS WORK!!! (1)
+            //NEEDS WORK!!! (1)
+            //NEEDS WORK!!! (1)
 
-        public:
-    };
     //Construct Font and Text Objects
     Font font;
     Text instruction_text;
@@ -54,22 +55,92 @@ int main()
     {
         CALCULATING, DISPLAYING
     };
-    
+    //Initialize it to CALCULATING
+    state state_variable = CALCULATING;
+            //  When the program starts or when the user clicks the mouse, 
+            //  we will calculate a new view for the complex plane
+            //  This is very CPU intense, so we will set the state variable
+            //  back to DISPLAYING after it is done so we only have to do it
+            //  once per click
+    //SOME USEFUL VARIABLES
+    Vector2i clicked;
 
-    //Declaring Variables
-    double real_number;
-    double imaginary_number;
-    complex<double> c (real_number, imaginary_number);
-    complex<double> z (0.0,0.0);
-    float view_center_real = 0.0f;
-    float view_center_imaginary = 0.0f;
-    float view_window_real_length = 2.0f;
-    float view_window_imaginary_length = 2.0f;
-    Vector2f center_point(view_center_real, view_center_imaginary);
-    Vector2f window_dim(view_window_real_length, view_window_imaginary_length);
-    View view1(center_point, window_dim);
+    //Begin the main loop
+    //MAIN GAME LOOP
+    while (window.isOpen())
+    {
+        //Handle Input segment
+        Event event;
+        
+            //Poll Windows queue events
+        while (window.pollEvent(event))
+        {
+            //Handle Event::Closed event to close the window
+            if(event.type == Event::Closed)
+            {
+                window.close();
+            }
 
-    //Running Calculations
+            //Handle Event::MouseButtonPressed
+            if (event.type == Event::MouseButtonPressed)
+            {
+                //NEEDS WORK!!! (2: 1)
+                //NEEDS WORK!!! (2: 1)
+                //NEEDS WORK!!! (2: 1)
+                //NEEDS WORK!!! (2: 1)
+                //NEEDS WORK!!! (2: 1)
+                //Something like this
+                    //Vector2f mouse = window.mapPixelToCoords(Vector2i(event.mouseButton.x, event.mouseButton.y));
+
+                //Right click will zoomOut and setCenter of the ComplexPlane object
+                if (event.mouseButton.button == Mouse::Right)
+                {
+                    //NEEDS WORK!!! (3: 1 and 2)
+                    //NEEDS WORK!!! (3: 1 and 2)
+                    //NEEDS WORK!!! (3: 1 and 2)
+                    //NEEDS WORK!!! (3: 1 and 2)
+                    //NEEDS WORK!!! (3: 1 and 2)
+                    
+                    //  Set the state to CALCULATING to generate
+                    //  the new complex plane view in the update segment
+                    //      LIKE THIS
+                    //      state_variable = CALCULATING;
+                }
+
+                //Left click will zoomIn and setCenter of the ComplexPlane object
+                if (event.mouseButton.button == Mouse::Left)
+                {
+                    //NEEDS WORK!!! (4: 1 and 2)
+                    //NEEDS WORK!!! (4: 1 and 2)
+                    //NEEDS WORK!!! (4: 1 and 2)
+                    //NEEDS WORK!!! (4: 1 and 2)
+                    //NEEDS WORK!!! (4: 1 and 2)
+
+                    //  Set the state to CALCULATING to generate
+                    //  the new complex plane view in the update segment
+                    //      LIKE THIS
+                    //      state_variable = CALCULATING;
+                }
+            }
+            
+            //Handle Event::MouseMoved
+            if (event.type == Event::MouseMoved)
+            {
+                //NEEDS WORK!!! (5: 1)
+                //NEEDS WORK!!! (5: 1)
+                //NEEDS WORK!!! (5: 1)
+                //NEEDS WORK!!! (5: 1)
+                //NEEDS WORK!!! (5: 1)
+
+                //  Use mapPixelToCoords to find the Vector2f coordinate
+                //  in the ComplexPlane View that corresponds to the screen mouse location
+                //      SOMETHING LIKE THIS
+                //      Vector2f mouse = window.mapPixelToCoords(Vector2i(event.mouseButton.x, event.mouseButton.y));
+                
+
+            }
+        }
+    }
 
 
     return 0;
