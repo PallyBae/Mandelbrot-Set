b30f1b846855848dd3e1176057e5650090837e96
Dom Piper <dominicpiper@gmail.com>
2022-09-29 12:01:00 -0700

    Commit from Ubuntu VM: Finally got it working!!

--- code/Main.cpp
+++ code/Main.cpp
@@ -31,12 +31,12 @@ int main()
     float windowScale = 0.8f;
 
     //!!! Same here! Can call the following 2 variables in other parts to avoid having to use any object member accessors
-    float currentWindowWidth  = sf::VideoMode::getDesktopMode().width  * windowScale; 
-    float currentWindowHeight = sf::VideoMode::getDesktopMode().height * windowScale;
+    int currentWindowWidth  = sf::VideoMode::getDesktopMode().width  * windowScale; 
+    int currentWindowHeight = sf::VideoMode::getDesktopMode().height * windowScale;
 
     //  Get the desktop resolution using:
     sf::VideoMode desktop(currentWindowWidth, currentWindowHeight); //!!! Changed to reflect the above changes
-    //  Calculate the aspect ratioof the moniter
+    //  Calculate the aspect ratio of the moniter
     float aspect_ratio = currentWindowWidth / currentWindowHeight; //!!! Changed to reflect the above changes
     //  Construct your window
     sf::RenderWindow window(desktop, "Mandelbrot Set!!", sf::Style::Default);
@@ -57,10 +57,9 @@ int main()
     textBoxBg.setPosition( {instruction_text.getPosition().x - TEXTBOX_BKG_W_ALIGN - textBoxBgBorder, instruction_text.getPosition().y + TEXTBOX_BKG_H_ALIGN - textBoxBgBorder});
 
     //  Construct a VertexArray
-    sf::VertexArray vertex_array(sf::Points);
-    int pixelWidth = 1;
+    sf::VertexArray vertex_array(sf::Points, currentWindowWidth*currentWindowHeight);
 
-    vertex_array.resize(currentWindowWidth*currentWindowHeight*pixelWidth); //!!! using the 2 window vars made this line a bit cleaner imo
+    vertex_array.resize(currentWindowWidth*currentWindowHeight); //!!! using the 2 window vars made this line a bit cleaner imo
 
     //  Create an enum class state variable with states CALCULATING and DISPLAYING
     enum state 
@@ -80,7 +79,6 @@ int main()
     sf::Vector2i clicked;
     sf::Vector2f mouse;
     sf::Vector2f pixel_location;
-    int point_counter = 0;
     size_t iterations_storage = 0;
 
     /*----------------------------------*\
@@ -103,57 +101,51 @@ int main()
             switch (event.type) //!!! This is kind of unnecessary but allows easy handling of other events
             {
             case sf::Event::Closed:
+            {
                 window.close();
                 break;
+            }
 
             case sf::Event::KeyPressed:
+            {
                 // Check if Keyboard:isKeyPRessed(Keyboard:Escape) to close the window
                 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-                {
                     window.close();
-                }
                 break;
+            }
 
             case sf::Event::MouseButtonPressed:
-                //Handle Right Mouse button press, which zooms out and centers the view
+            {
+                //  Use mapPixelToCoords to find the Vector2f coordinate in the Complex Plane View
+                //  that corresponds to the screen mouse click
+                mouse = window.mapPixelToCoords({ event.mouseButton.x, event.mouseButton.y }, c_plane.getView());
+
                 if (event.mouseButton.button == sf::Mouse::Right)
-                {
-                    //  Use mapPixelToCoords to find the Vector2f coordinate in the Complex Plane View
-                    //  that corresponds to the screen mouse click
-                    mouse = window.mapPixelToCoords(sf::Vector2i(event.mouseButton.x, event.mouseButton.y));
-
-                    //  Right click will zoomOut and setCenter of the ComplexPlane object
-                    c_plane.setCenter(mouse);
                     c_plane.zoomOut();
-                        
-                    //  Set the state to CALCULATING to generate
-                    //  the new complex plane view in the update segment
-                    state_variable = CALCULATING;
-                }
-                //  Left click will zoomIn and setCenter of the ComplexPlane object
-                if (event.mouseButton.button == sf::Mouse::Left)
-                {
-                    mouse = window.mapPixelToCoords(sf::Vector2i(event.mouseButton.x, event.mouseButton.y), c_plane.getView());
 
-                    //Left click will zoomIn and setCenter of the ComplexPlane object
-                    c_plane.setCenter(mouse);
+                if (event.mouseButton.button == sf::Mouse::Left)
                     c_plane.zoomIn();
 
-                    //  Set the state to CALCULATING to generate
-                    //  the new complex plane view in the update segment
-                    state_variable = CALCULATING;
-                }
+                //After we zoom in or out, make sure to set center of the Complex Plane.
+                c_plane.setCenter(mouse);
+
+                //  Set the state to CALCULATING to generate
+                //  the new complex plane view in the update segment
+                state_variable = CALCULATING;
                 break;
+            }
             
             case sf::Event::MouseMoved:
+            {
                 //  Use mapPixelToCoords to find the Vector2f coordinate
                 //  in the ComplexPlane View that corresponds to the screen mouse location
-                mouse = window.mapPixelToCoords(sf::Vector2i(event.mouseButton.x, event.mouseButton.y));
+                mouse = window.mapPixelToCoords(sf::Vector2i(event.mouseButton.x, event.mouseButton.y)), c_plane.getView();
 
                 //  setMouseLocation on the ComplexPLane object to store this coordinate
                 //  This will be used later to display the mouse coordinates as it moves
                 c_plane.setMouseLocation(mouse);
                 break;
+            }
                 
             default: //!!! Added this to handle misc events
                 break;
@@ -177,24 +169,24 @@ int main()
             int percentUpdate = 0;
             int prevPercent = 0;
             //point_counter = 0;
-            for (int i = 0; i < static_cast<int>(currentWindowHeight); i++) //!!! using the new currentWindowHeight var
+            for (int j = 0; j < currentWindowWidth; j++) //!!! using the new currentWindowHeight var
             {
-                percentUpdate = (i / currentWindowHeight) * 100;
+                percentUpdate = (j / static_cast<float>(currentWindowWidth)) * 100.0;
                 if (percentUpdate != prevPercent && percentUpdate % 5 == 0)
                 {
                     std::cout << "Percent: " << percentUpdate << std::endl;
                 }
 
-                for(int j = 0; j < static_cast<int>(currentWindowWidth); j++) //!!! using the new currentWindowWidth var
+                for(int i = 0; i < currentWindowHeight; i++) //!!! using the new currentWindowWidth var
                 {
                     //  Set the position variable in the element of VertexArray
                     //  that corresponds to the screen coordinate j,i
                     //  This is difficult, so study the line of code below:
-                    vertex_array[j+i*static_cast<int>(currentWindowWidth)].position = {(float)j,(float)i};
+                    vertex_array[j+i*currentWindowWidth].position = {(float)j,(float)i};
 
                     //  Use mapPixelToCoords to find the Vector2f coordinate in the ComplexPlane View
                     //  that corresponds to the screen pixel location at j,i
-                    pixel_location = window.mapPixelToCoords(sf::Vector2i(j, i));
+                    pixel_location = window.mapPixelToCoords(sf::Vector2i(j, i),c_plane.getView());
 
                     //  Call ComplexPlane::countIterations for the Vector2f coordinate in the ComplexPlane and store the number of iterations
                     iterations_storage = ComplexPlane::countIterations(pixel_location);//!!! Infinite loop is here!!!
@@ -210,7 +202,7 @@ int main()
                     ComplexPlane::iterationsToRGB(iterations_storage, r, g, b);
 
                     //  Set the color variable in the element of VertexArray that corresponds to the screen coordinate j,i
-                    vertex_array[j+i*pixelWidth].color = {r,g,b};
+                    vertex_array[j+i*static_cast<int>(currentWindowWidth)].color = {r,g,b};
                 }
                 prevPercent = percentUpdate;
 
